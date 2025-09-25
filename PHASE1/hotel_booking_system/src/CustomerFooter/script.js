8bda1cd76827bda10d47fa831034bdd5e35fb739
Aswin-Nath <aswinnathte125@gmail.com>
2025-09-26 02:07:20 +0530

    Deleted the Old Files

--- PHASE1/hotel_booking_system/src/CustomerFooter/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("../CustomerFooter/index.html") // adjust path based on where footer.html is saved
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("footer").innerHTML = data;
-    })
-    .catch(err => console.error("Footer load failed:", err));
-});
